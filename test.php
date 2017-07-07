83fb7fad6f84502e782bd0f274a1e8e2743cd85d
willmont21 <willmont66@gmail.com>
2017-07-07 00:56:46 -0500

    Update test.php

--- test.php
+++ test.php
@@ -1,11 +1,13 @@
 <?php
 use PHPUnit\Framework\TestCase;
 
+require_once "hello_world.php";
+
 class Evaluate extends TestCase {
 
   public function testHelloWorld() {
     ob_start();
-    require "hello_world.php";
+    helloWorld();
     $output = ob_get_clean();
     $this->assertEquals("Hello World!", $output, "You must print Hello World");
   }
