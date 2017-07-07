914157d35412f58650320194e986a14385a516f4
willmont21 <willmont66@gmail.com>
2017-07-07 12:45:57 -0500

    Update CalculatorTest.php

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -5,16 +5,6 @@ class CalculatorTests extends PHPUnit_Framework_TestCase
 {
     private $calculator;
  
-    protected function setUp()
-    {
-        $this->calculator = new Calculator();
-    }
- 
-    protected function tearDown()
-    {
-        $this->calculator = NULL;
-    }
- 
     public function testAdd()
     {
         $result = $this->calculator->add(1, 2);
