9da3c711e99c70f0eb60b9ffc7fa6723036b2b95
lisscontreras <lisscb1104@gmail.com>
2017-07-07 12:24:58 -0500

    Update Calculator.php

--- Calculator.php
+++ Calculator.php
@@ -1,18 +1,10 @@
 <?php
-// Calculator.php
-class Calculator {
-    public function getNumberFromUserInput() {
-        // complicated function to get number from user input
+class Calculator
+{
+ 
+    public function add($a, $b)
+    {
+        return $a + $b;
     }
-    public function printToScreen($value) {
-        // another complicated function
-    }
-    public function divideBy($num2) {
-        if ($num2 == 0) return NAN;
-        return $this->getNumberFromUserInput()/$num2;
-    }
-    public function divideByAndPrint($num2) {
-        if ($num2 == 0) $this->printToScreen("NaN");
-        $this->printToScreen($this->getNumberFromUserInput()/$num2);
-    }
+ 
 }
