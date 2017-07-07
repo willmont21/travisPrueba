5389e1d40add334b51330c1b6eb73022f37cf038
lisscontreras <lisscb1104@gmail.com>
2017-07-07 12:23:53 -0500

    Update CalculatorTest.php

--- CalculatorTest.php
+++ CalculatorTest.php
@@ -1,36 +1,24 @@
 <?php
-// CalculatorTest.php
-include_once("Calculator.php");
-class CalculatorTest extends \PHPUnit_Framework_TestCase {
-    public function testDivideByPositiveNumber() {
-        $calcMock=$this->getMock('\Calculator',array('getNumberFromUserInput'));
-        $calcMock->expects($this->once())
-            ->method('getNumberFromUserInput')
-            ->will($this->returnValue(10));
-        $this->assertEquals(5,$calcMock->divideBy(2));
+require 'Calculator.php';
+ 
+class CalculatorTests extends PHPUnit_Framework_TestCase
+{
+    private $calculator;
+ 
+    protected function setUp()
+    {
+        $this->calculator = new Calculator();
     }
-    public function testDivideByZero() {
-        $calcMock=$this->getMock('\Calculator',array('getNumberFromUserInput'));
-        $calcMock->expects($this->never())
-            ->method('getNumberFromUserInput')
-            ->will($this->returnValue(10));
-        $this->assertEquals(NAN, $calcMock->divideBy(0));
+ 
+    protected function tearDown()
+    {
+        $this->calculator = NULL;
     }
-    public function testDivideByNegativeNumber() {
-        $calcMock=$this->getMock('\Calculator',array('getNumberFromUserInput'));
-        $calcMock->expects($this->once())
-            ->method('getNumberFromUserInput')
-            ->will($this->returnValue(10));
-        $this->assertEquals(-2,$calcMock->divideBy(-5));
+ 
+    public function testAdd()
+    {
+        $result = $this->calculator->add(1, 2);
+        $this->assertEquals(3, $result);
     }
-    public function testDivideByPositiveNumberAndPrint() {
-        $calcMock=$this->getMock('\Calculator',array('getNumberFromUserInput', 'printToScreen'));
-        $calcMock->expects($this->once())
-            ->method('getNumberFromUserInput')
-            ->will($this->returnValue(10));
-        $calcMock->expects($this->once())
-            ->method('printToScreen')
-            ->with($this->equalTo('5'));
-        $calcMock->divideByAndPrint(2);
-    }
+ 
 }
